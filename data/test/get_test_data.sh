531cfe43eba20c4d9ce4b557c3a288e692ea8e5f
Finlay Maguire <finlaymaguire@gmail.com>
2020-06-27 22:45:41 -0300

    Add klebsiella test data

--- data/test/get_test_data.sh
+++ data/test/get_test_data.sh
@@ -4,3 +4,6 @@ set -o errexit
 
 wget -O SAMN02599008.tar.gz  https://osf.io/6ma8p/download
 tar xvf SAMN02599008.tar.gz 
+
+wget -O SAMEA6634591.tar.gz  https://osf.io/4tqxc/download
+tar xvf SAMEA6634591.tar.gz
