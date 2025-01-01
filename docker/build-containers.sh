de52ede6d21b1a48bfadc7ce2f5a7ae17bf039ea
Marco van Zwetselaar <io@zwets.it>
2025-01-01 21:56:22 +0300

    WIP: add final Dockerfile

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 docker build -f docker/Dockerfile.snake-only -t hamronization_workflow-snake . &&
-docker build -f docker/Dockerfile.with-envs -t hamronization_workflow-almost .
+docker build -f docker/Dockerfile.with-envs -t hamronization_workflow-almost . &&
+docker build -f docker/Dockerfile -t hamronization_workflow .
