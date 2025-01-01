39581f8a81ee73046d25562c30c7697e7beae93e
Marco van Zwetselaar <io@zwets.it>
2025-01-01 23:12:16 +0300

    Fix container names

--- docker/run-with-envs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LC_ALL="C"
-set -euo pipefail
-cd "$(dirname "$0")/.."
-
-exec docker run -it --rm --tmpfs /.cache --tmpfs /run --tmpfs /tmp -v "$PWD/data:/data:ro" hamronization_workflow-almost "$@"
