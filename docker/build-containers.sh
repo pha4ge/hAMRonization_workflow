27fd84c5fc6660626c00da1cb90a81398afed0ca
Marco van Zwetselaar <io@zwets.it>
2025-01-01 23:12:16 +0300

    Fix container names

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -4,6 +4,6 @@ export LC_ALL="C"
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
-docker build -f docker/Dockerfile.snake-only -t hamronization_workflow-snake . &&
-docker build -f docker/Dockerfile.with-envs -t hamronization_workflow-almost . &&
+docker build -f docker/Dockerfile.snake-only -t hamronization_workflow-snake-only . &&
+docker build -f docker/Dockerfile.no-data -t hamronization_workflow-no-data . &&
 docker build -f docker/Dockerfile -t hamronization_workflow .
