c87ea1a31a5f15aeed4b41a36194d9f7351ffdbd
Marco van Zwetselaar <io@zwets.it>
2025-01-04 02:31:02 +0300

    Docker: add build step 0

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -4,6 +4,7 @@ export LC_ALL="C"
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
+docker build -f docker/Dockerfile-step-0 -t hamronization_workflow-step-0 . &&
 docker build -f docker/Dockerfile-step-1 -t hamronization_workflow-step-1 . &&
 docker build -f docker/Dockerfile-step-2 -t hamronization_workflow-step-2 . &&
 docker build -f docker/Dockerfile -t hamronization_workflow .
