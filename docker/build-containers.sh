5fb8b43703c9955684038a706a902ba37cabd282
Marco van Zwetselaar <io@zwets.it>
2025-01-04 00:31:11 +0300

    Make stepped Dockerfiles

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -4,6 +4,6 @@ export LC_ALL="C"
 set -euo pipefail
 cd "$(dirname "$0")/.."
 
-docker build -f docker/Dockerfile.snake-only -t hamronization_workflow-snake-only . &&
-docker build -f docker/Dockerfile.no-data -t hamronization_workflow-no-data . &&
+docker build -f docker/Dockerfile-step-1 -t hamronization_workflow-step-1 . &&
+docker build -f docker/Dockerfile-step-2 -t hamronization_workflow-step-2 . &&
 docker build -f docker/Dockerfile -t hamronization_workflow .
